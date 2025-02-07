fix sign initialization

src/graphesn.py
@@ -4,7 +4,6 @@ import torch.nn.functional as F
 
 import initializers
 
-#TODO: change the way the weight matrices are initialized
 #TODO: after that check the GES property (Graph Embedding Stability)
 
 class GraphReservoir(MessagePassing):
@@ -39,13 +38,17 @@ class GraphReservoir(MessagePassing):
         input_initializer = getattr(initializers, input_initializer)
         recurrent_initializer = getattr(initializers, recurrent_initializer)
 
-        self.W_in = torch.nn.Parameter(input_initializer(torch.Size([hidden_size, input_size])), requires_grad=True)
-        self.W_h = torch.nn.Parameter(recurrent_initializer(torch.Size([hidden_size, hidden_size])), requires_grad=True)
+        self.W_in = torch.nn.Parameter(input_initializer(torch.Size([hidden_size, input_size])), requires_grad=False)
+        self.W_h = torch.nn.Parameter(recurrent_initializer(torch.Size([hidden_size, hidden_size])), requires_grad=False)
+
+        # scale the weight matrices
+        self.W_in.mul_(omhega).float() 
+        self.W_h.div_(torch.linalg.eigvals(self.W_h).abs().max()).mul_(rho).float()
 
 
     
     @torch.no_grad()
-    def forward(self, x:torch.Tensor, edge_index:torch.Tensor, x_neighbors:torch.Tensor = None, threshold:float=1e-3, max_steps:int=100) -> torch.Tensor:
+    def forward(self, x:torch.Tensor, edge_index:torch.Tensor, x_neighbors:torch.Tensor = None, threshold:float=1e-3, max_steps:int=1000) -> torch.Tensor:
         '''
         Forward pass of the Graph Reservoir Network. The formula for the forward pass is given by:
         x = tanh(W_in @ u_in + tanh(W_h @ sum_{j \in N(i)} x_j))

src/initializers.py
@@ -1,11 +1,9 @@
 import torch
-from math import pi
-
-#TODO: Implement the rescaling of the weights matrices
+import mpmath
 
 def uniform(size:torch.Size,
-            min_val:float, 
-            max_val:float) -> torch.Tensor:
+            min_val:float=-1.0, 
+            max_val:float=1.0) -> torch.Tensor:
     '''
     Initialize a tensor with uniform distribution.
 
@@ -68,9 +66,10 @@ def sign(size:torch.Size) -> torch.Tensor:
     torch.Tensor
         The initialized tensor
     '''
-    
-    pi_str = str(torch.tensor(pi).item())[2:2+(size[0]*size[1])]
-    pi_matrix = torch.tensor([1 if int(digit) >= 5 else -1 for digit in pi_str])
+    num_digits = size.numel() + 2
+    mpmath.mp.dps = num_digits
+    pi_str = mpmath.nstr(mpmath.pi, num_digits)[2:num_digits]
+    pi_matrix = torch.tensor([1 if int(digit) >= 5 else -1 for digit in pi_str], dtype=torch.float32)
 
     return pi_matrix.reshape(size)
 