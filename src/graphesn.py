9bb69b5e1df34e41f3bb11b6dcc0664451bc5395
JacopoRaffi <raffijacopo@gmail.com>
2025-02-07 16:12:31 +0100

    implemented forward pass

--- src/graphesn.py
+++ src/graphesn.py
@@ -1,18 +1,66 @@
 from torch_geometric.nn import MessagePassing
 import torch.nn
+import torch.nn.functional as F
+
+#TODO: change the way the weight matrices are initialized
+#TODO: after that check the GES property (Graph Embedding Stability)
 
 class GraphReservoir(MessagePassing):
-    def __init__(self):
-        pass
+    def __init__(self, input_size, hidden_size):
+        super(GraphReservoir, self).__init__(aggr='add')
+        
+        self.input_size = torch.nn.Parameter(torch.tensor(input_size), requires_grad=False)
+        self.hidden_size = torch.nn.Parameter(torch.tensor(hidden_size), requires_grad=False)
+
+        self.W_in = torch.nn.Parameter(torch.randn(hidden_size, input_size), requires_grad=False)
+        self.W_h = torch.nn.Parameter(torch.randn(hidden_size, hidden_size), requires_grad=False)
     
-    def forward(self, x, edge_index):
-        pass
+    @torch.no_grad()
+    def forward(self, x:torch.Tensor, edge_index:torch.Tensor, x_neighbors:torch.Tensor = None, threshold:float=1e-3, max_steps:int=100) -> torch.Tensor:
+        '''
+        Forward pass of the Graph Reservoir Network. The formula for the forward pass is given by:
+        x = tanh(W_in @ u_in + tanh(W_h @ sum_{j \in N(i)} x_j))
+        where:
+            x : node embeddings
+            u_in : input node features
+            W_in : input weight matrix
+            W_h : recurrent weight matrix
+            N(i) : set of neighbours of node i
+            x_j : node embedding of the j-th neighbour
 
-    def message(self, x_j):
-        pass
+        Parameters
+        ----------
+        x : torch.Tensor
+            The input tensor of shape [N, input_size] where N is the number of nodes in the graph
+        edge_index : torch.Tensor
+            The edge index tensor of shape [2, E] where E is the number of edges in the graph
+        x_neighbors : torch.Tensor, optional
+            The initial node embeddings of the neighbours, by default None
+        threshold : float, optional
+            The threshold for the convergence of the node embeddings, by default 1e-3
+        max_steps : int, optional
+            The maximum number of steps for the convergence of the node embeddings, by default 100
+        
+        Returns
+        -------
+        torch.Tensor
+            The node embeddings of the graph of shape [N, hidden_size]
+        '''
+        u_in = x.clone()
+        x_old = torch.zeros(x.shape[0], self.hidden_size) if x_neighbors is None else x_neighbors
+        norm = float('inf')
 
-    def update(self, aggr_out):
-        pass
+        while (norm > threshold) and (max_steps > 0):
+            x = F.linear(x_old, self.W_h) # apply recurrent matrix to the node embeddings
+            neighbors = self.propagate(edge_index, x=x) # message passing phase
+
+            x = F.tanh(F.linear(u_in, self.W_in) + neighbors) # update the node embeddings
+
+            norm = torch.norm(x - x_old, p=2)
+            x_old = x.clone()
+            max_steps -= 1
+
+        return x
 
 
 class GraphESN(torch.nn.Module):
